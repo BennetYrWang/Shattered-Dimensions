0d88e4a86c71c1b025ed1d968e998dc02dca66c2
BennetYrWang <107472601+BennetYrWang@users.noreply.github.com>
2024-11-05 12:28:45 -0500

    Dash and facing each other

--- Assets/Scripts/Bennet/MovementSystem/PlayerMovementController.cs
+++ Assets/Scripts/Bennet/MovementSystem/PlayerMovementController.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Bennet.MovementSystem
 {
     public class PlayerMovementController : MonoBehaviour
     {
-        public KeyCode toLeft;
-        public KeyCode toRight;
+        [FormerlySerializedAs("toLeft")] public KeyCode toLeftKey;
+        [FormerlySerializedAs("toRight")] public KeyCode toRightKey;
         public KeyCode jump;
         public float moveSpeed = 1f;
         public float jumpVelocity = 3f;
-        public PlayerActor body, illusion;
 
+
+        public bool isDashing = false;
+        public bool dashRecovered = true;
+        public bool canDash => dashRecovered;
+        
+        public bool canDashInterrupted = false;
+        
+        public float dashSpeed = 3f;
+        public float dashDurationSecond = 0.2f;
+        public float doubleClickIntervalSecond = 0.15f;
+        private float doubleClickCounter = -1f;
+        private float dashRecoveryCounterSecond = -1f;
+        private float dashDurationCounterSecond = -1f;
+        [FormerlySerializedAs("dashRecoveryTimeSecond")] public float dashRecoverySecond = 1f;
+        
+        
+        public PlayerActor body, illusion;
+        
         private bool _dualExistence = true;
 
+        private KeyCode pressedControllKey;
 
         public bool DualExistence
         {
@@ -26,38 +45,79 @@ namespace Bennet.MovementSystem
 
         private void Update()
         {
-            print(Time.time);
+            print(pressedControllKey);
+            // Double Click Stuff
+            if (dashRecoveryCounterSecond > 0f)
+            {
+                dashRecoveryCounterSecond -= Time.deltaTime;
+                if (dashRecoveryCounterSecond < 0f)
+                {
+                    dashRecoveryCounterSecond = -1f;
+                    dashRecovered = true;
+                }
+            }
+
+            if (doubleClickCounter > 0f)
+            {
+                doubleClickCounter -= Time.deltaTime;
+                if (doubleClickCounter < 0f)
+                {
+                    pressedControllKey = KeyCode.None;
+                    doubleClickCounter = -1f;
+                }
+            }
+
+            if (dashDurationCounterSecond > 0f)
+            {
+                dashDurationCounterSecond -= Time.deltaTime;
+                if (dashDurationCounterSecond < 0f)
+                {
+                    StopDashing();
+                }
+            }
+            
+            // Get input
             float movement = 0;
-            if (Input.GetKey(toLeft))
+            if (isDashing)
             {
-                body.FlipCharacter(true);
-                illusion.FlipCharacter(true);
-                movement -= moveSpeed;
+                movement = illusion.FacingRight ? dashSpeed : -dashSpeed;
+            }
+            else
+            {
+                if (Input.GetKeyDown(toLeftKey))
+                    RegisterPressedKey(toLeftKey);
 
+                if (Input.GetKeyDown(toRightKey))
+                    RegisterPressedKey(toRightKey);
+
+                if (!isDashing)
+                {
+                    if (Input.GetKey(toLeftKey))
+                    {
+                        movement -= moveSpeed;
+                        if (_dualExistence)
+                            illusion.FacingRight = false;
+                    }
+
+                    if (Input.GetKey(toRightKey))
+                    {
+                        movement += moveSpeed;
+                        if (_dualExistence)
+                            illusion.FacingRight = true;
+                    }
+                }
             }
 
-            if (Input.GetKey(toRight))
-            {
-                body.FlipCharacter(false);
-                illusion.FlipCharacter(false);
-                movement += moveSpeed;
-
-            }
-
-
-            TryMovePlayerHorizontally(movement * Time.deltaTime);
+            bool moveSucceed = TryMovePlayerHorizontally(movement * Time.deltaTime);
+            if (isDashing && moveSucceed && canDashInterrupted)
+                StopDashing();
 
             if (Input.GetKeyDown(jump))
             {
                 body.Jump(jumpVelocity);
-                illusion.Jump(jumpVelocity);
-
+                if (_dualExistence)
+                    illusion.Jump(jumpVelocity);
             }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-                DualExistence = !DualExistence;
-
-
 
         }
 
@@ -75,6 +135,34 @@ namespace Bennet.MovementSystem
             return true;
         }
 
+        private void RegisterPressedKey(KeyCode dashKey)
+        {
+            if (!canDash)
+                return;
+            if (pressedControllKey != dashKey)
+            {
+                pressedControllKey = dashKey;
+                doubleClickCounter = doubleClickIntervalSecond;
+                return;
+            }
 
+            doubleClickCounter = -1f;
+            StartDashing();
+        }
+        private void StartDashing()
+        {
+            print("Dash");
+            isDashing = true;
+            dashRecovered = false;
+            dashDurationCounterSecond = dashDurationSecond;
+            dashRecoveryCounterSecond = dashRecoverySecond;
+            pressedControllKey = KeyCode.None;
+        }
+
+        private void StopDashing()
+        {
+            isDashing = false;
+            dashDurationCounterSecond = -1f;
+        }
     }
 }
---
+++ Assets/Scripts/Bennet/MovementSystem/PlayersManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bennet.MovementSystem
+{
+    public class PlayersManager : MonoBehaviour
+    {
+        public PlayerMovementController p1, p2;
+
+        private void FixedUpdate()
+        {
+            float horizontalP1ToP2 = Vector3.Dot(p1.body.transform.position - p2.body.transform.position,
+                p1.body.GetForwardDirection());
+            p1.body.FacingRight = horizontalP1ToP2 >= 0;
+            p2.body.FacingRight = horizontalP1ToP2 < 0;
+        }
+    }
+}
